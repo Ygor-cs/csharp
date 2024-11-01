6c1ca35f2c63a71a8ce8beaf321dc339e3c167d6
lucasfigerno <164235144+lucasfigerno@users.noreply.github.com>
2024-11-01 17:15:27 -0400

    Operadores Aritméticos

--- operadores/Program.cs
+++ operadores/Program.cs
@@ -1,2 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+// Operadores Aritmeticos 
+adição +
+subtrção - 
+multiplicação *
+divisão /
+
+mod % busca na dvisão o seu resto!
+exponenciação ** eleva um valor à potencia de outro 
+incremento ++ aumenta o valor da variavel ou numero em 1 
+decremento -- diminui o valor da variavel ou em numero 1 
+
+3 school
+
+Console.WriteLine("Wello World!")
+double resultado = 12 + 8 + 12 / 3; 
+Console.WriteLine("resultado");
+
+double resultado = (12 + 8 + 12) / 3; 
+Console.WriteLine("resultado");
+
+double resultado = (2 + 8) * 2 / 4; 
+Console.WriteLine("resultado");
+
