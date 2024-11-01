eccc7d2062665a457ce7cab203e0225f7182bf77
lucasfigerno <164235144+lucasfigerno@users.noreply.github.com>
2024-11-01 16:31:24 -0400

    Tipos de dados não primitivos

--- variaveis/Program.cs
+++ variaveis/Program.cs
@@ -17,3 +17,42 @@ char inical = "w";
 // Tipos Logicos 
 bool estaLogado = true;
 bool ativo = false; 
+
+// Tipos de dados não primitivos 
+string saudacao = "Ola mundo!";
+
+//Arrays
+int[] Numeros = {1, 2, 3, 4, 5};
+string[] nomes = {"Ygor", "Ana Clara", "Mariangela"};
+
+// Classes
+public class Pessoa 
+{
+    public string Nome;
+    public int idade;   
+}
+
+// Structs  
+public class Ponto 
+{
+    public int X;
+    public int Y;   
+}
+
+// Enums 
+public enum diaDaSemana 
+{
+    Segunda, Terça, Quarta, Quinta, Sexta, Sabado, Domingo
+}
+
+// Interfaces 
+public interface IVeiculo 
+{
+    void Mover();
+}
+
+// Delegates 
+public void Delegates Processar(int Numero);
+
+// Nullable types 
+int? idade = null; 
