45178f21aab9d0d894fe1eb074eb5b29af8dca9e
Ygor-cs <decarvalhosouzaygor@gmail.com>
2024-11-04 17:26:15 -0400

    Operadores Type

--- concatenacao/Program.cs
+++ concatenacao/Program.cs
@@ -14,3 +14,25 @@ incorporar variaveis diretamente em uma string
 string nome1 = "Ygor";
 string saudacao1 = $"Olá, {nome1}! Bem vindo.";
 Console.WriteLine(saudacao1);
+
+/* Operador GetType()
+ retornapara saber qual é o tipo de variável q estamos usando
+*/
+int numero = 5;
+Console.WriteLine(numero.GetType());
+
+/* Palavra-chave typeof
+ usado para obter o tipo de classe ou estrutura 
+*/
+Console.WriteLine(typeof(int));
+
+/* Metódo Parse 
+ fazer converção de uma string que tenha um valor de número 
+*/ 
+string valorTexto = "100";
+Console.WriteLine("A variável valorTexto é do tipo:" + valorTexto.GetType());
+
+int numero1 = int.Parse(valorTexto);
+Console.WriteLine(numero1);
+Console.WriteLine("A variável numeroParse é do tipo:" + numero1.GetType());
+
