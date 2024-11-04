7124d9a70a20d674548539393276ed3288752da4
Ygor-cs <decarvalhosouzaygor@gmail.com>
2024-11-04 15:26:17 -0400

    Add Operadores Lógicos

--- operadores/Program.cs
+++ operadores/Program.cs
@@ -1,4 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Security.Cryptography;
+
 Console.WriteLine("Hello, World!");
 
 /* Operadores Aritmeticos 
@@ -25,3 +27,57 @@ Console.WriteLine(resultado2);
 double resultado3 = (2 + 8) * 2 / 4; 
 Console.WriteLine(resultado3);
 
+/* Operadores de comparação
+igual ==
+diferente !=
+maior ou igual >=
+menor ou igual <=
+*/
+
+/* Operadores lógicos 
+utilizados para expressões booleanas 
+
+operadores em c# 
+AND Lógico && (E)
+ só retorna true apenas se ambas forem verdadeiras - true 
+
+OR Lógico || (OU)
+ retorna true se pelo menos umas das duas expressões forem verdadeiras - true 
+
+NOT Lógico ! (NÃO)
+ inverte o valor logico da expressão, se valor é verdadeiro se torna falso e vice-versa
+
+XOR Lógico ^(OU Exclusivo)
+ retorna true somente se uma das expressões for true e a outra for false  
+*/ 
+
+//AND Logico 
+ bool a = true;
+ bool b = false;
+ Console.WriteLine(a && b);
+
+//OR Logico
+ bool a1 = true;
+ bool b1 = false;
+ Console.WriteLine(a1 || b1); 
+
+//NOT Logico
+ bool a2 = true;
+ Console.WriteLine(!a2);
+
+//XOR Logico ^ (OU Exclusivo)
+ bool a3 = true;
+ bool b3 = false;
+ Console.WriteLine(a3^b3);
+
+ bool a4 = true;
+ bool b4 = false; 
+ bool c = false; 
+ bool resultado = !a4 || (b4 && c);
+ Console.WriteLine(resultado);
+
+bool a5 = false; 
+bool b5 = false; 
+bool c1 = false; 
+bool resultado4 = !a5 || b5 && c1;
+Console.WriteLine(resultado4);
