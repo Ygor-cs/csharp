96f712edc339e5046d77bd11172fe02f2a568069
Ygor-cs <decarvalhosouzaygor@gmail.com>
2024-11-04 16:55:05 -0400

    Add projeto sobre concatenação

---
+++ concatenacao/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+/*
+Concatenação
+ unir duas ou mais strings
+*/
+string nome = "Ygor";
+string saudacao = "Olá, " + nome + "! Bem-vindo.";
+Console.WriteLine(saudacao);
+
+    
