7fe9e88115a22798fad341b8c50b3c77907abd3d
Ygor-cs <decarvalhosouzaygor@gmail.com>
2024-11-11 14:33:07 -0400

    Add exemplos estrutura repetição: While

---
+++ estrutura-de-repeticao/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+/*Estrutura de repeticao (Loop)
+While
+Do While 
+For
+Foreach
+*/
+Random numeroAleatorio = new Random();
+int numero = numeroAleatorio.Next(1, 101);
+int contador = 0;
+Console.WriteLine($"Número Alvo: {contador}");
+// Loop que incrementa o contador até ele atingir o número alvo
+while ( contador <= numero )
+{
+    Console.WriteLine($"Contador: {contador}.");
+    contador ++;
+}
+Console.WriteLine($"Contador atingiu o número alvo: {contador}");
